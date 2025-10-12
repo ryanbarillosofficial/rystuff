570ff5fe2de03661884ad7943ea5f644a99187a8
ryanbarillosofficial <129530399+ryanbarillosofficial@users.noreply.github.com>
2025-10-12 06:05:12 +0300

    Updates

--- setup.sh
+++ setup.sh
@@ -10,9 +10,15 @@ ln -s /rystuff/etc/firefox /etc
 ln -s /rystuff/etc/initcpio/post/copy-kernel-and-initramfs /etc/initcpio/post/
 
 # Pacman
-rm /etc/pacman.conf /etc/pacman.d/mirrorlist
-ln -s /rystuff/etc/pacman.conf /etc/
-ln -s /rystuff/etc/pacman.d/mirrorlist /etc/pacman.d/
+rm /etc/pacman.conf /etc/pacman.d/mirrorlist && ln -s /rystuff/etc/pacman.d/mirrorlist /etc/pacman.d/
+# ln -s /rystuff/etc/pacman.conf /etc/
 
+# Looking Glass
+
+# Flatpak update service
+ln -s /rystuff/etc/systemd/system/flatpak-update.service /etc-systemd/system/
+ln -s /rystuff/etc/systemd/system/flatpak-update.timer /etc-systemd/system/
+systemctl daemon-reload
+systemctl enable --now flatpak-update.timer
 
 
